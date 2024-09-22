6cc91b40b6c5efe9726d753f9355586b90e0d350
0ndrec <byeexs@gmail.com>
2024-09-22 22:36:53 +0300

    _

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+import os
 from utils.account import new_encrypt_token, KeyManager
 from dotenv import load_dotenv, dotenv_values, set_key
-from getpass import getpass
 from colorama import Fore, Back, Style
 from web3 import Web3
-
-
+import inquirer
 
 load_dotenv()
 config = dotenv_values(".env")
 
-
-#____________________________ENCRYPTION_TOKEN_SECTION________________________________
-if config["ENCRYPTION_TOKEN"] == "" or config["ENCRYPTION_TOKEN"] is None:
-    print("Generating a new encryption token for safety storing private keys...")
+# ____________________________ENCRYPTION_TOKEN_SECTION________________________________
+if config.get("ENCRYPTION_TOKEN") == "" or config.get("ENCRYPTION_TOKEN") is None:
+    print("Generating a new encryption token for safely storing private keys...")
     # Generate a new encryption token for storing private keys
     __token = new_encrypt_token().decode()
     # Save the new encryption token in the .env file
@@ -20,112 +18,206 @@ if config["ENCRYPTION_TOKEN"] == "" or config["ENCRYPTION_TOKEN"] is None:
     km = KeyManager(config["KEYS_PATH"], __token)
 else:
     km = KeyManager(config["KEYS_PATH"], config["ENCRYPTION_TOKEN"])
-#____________________________________________________________________________________
+# ____________________________________________________________________________________
 
 
-
-#______________________________INITIALIZE_WEB3_SECTION________________________
+# ______________________________INITIALIZE_WEB3_SECTION________________________
 def w3_init(endpoint) -> Web3:
     w3 = Web3(Web3.HTTPProvider(endpoint))
     if w3.is_connected():
-        print(f"{Back.GREEN}\nConnected to the endpoint, currently chain ID: {w3.eth.chain_id}{Style.RESET_ALL}")
+        print(
+            f"{Back.GREEN}\nConnected to the endpoint, current chain ID: {w3.eth.chain_id}{Style.RESET_ALL}"
+        )
     else:
-        print(f"{Back.RED}\n Failed to connect to the Ethereum endpoint.{Style.RESET_ALL}")
+        print(f"{Back.RED}\nFailed to connect to the Ethereum endpoint.{Style.RESET_ALL}")
     return w3
-#_____________________________________________________________________________
 
+
+# _____________________________________________________________________________
 
 
 def menu():
-    sentinel = '9'
+    sentinel = "Exit"
     choice = None
+    w3 = None  # Initialize w3 variable
     while choice != sentinel:
-        print("1. Restore an account from a passphrase")
-        print("2. Delete an account")
-        print("3. Get private key from an account")
-        print("4. Generate new account(s)")
-        print("5. Connect to endpoint")
-        print("6. Show my accounts")
-        print("7. Get balance of each account")
-        print("9. Exit")
+        # Clear the terminal at the beginning of each loop iteration
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-        choice = input("Enter your choice: ")
+        questions = [
+            inquirer.List(
+                "choice",
+                message="What do you want to do?",
+                choices=[
+                    "Restore an account from a passphrase",
+                    "Delete an account",
+                    "Get private key from an account",
+                    "Generate new account(s)",
+                    "Connect to endpoint",
+                    "Show my accounts",
+                    "Get balance of each account",
+                    "Exit",
+                ],
+            )
+        ]
+        answer = inquirer.prompt(questions)
+        choice = answer["choice"]
+
         match choice:
-            case '1':
-                name = input("Enter account name: ")
-                passphrase = getpass("Enter passphrase: ")
-                if len(passphrase.split(" ")) < 12 or len(passphrase.split(" ")) > 24:
+            case "Restore an account from a passphrase":
+                os.system('cls' if os.name == 'nt' else 'clear')  # Clear terminal before action
+                questions = [
+                    inquirer.Text("name", message="Enter account name"),
+                    inquirer.Password("passphrase", message="Enter passphrase"),
+                ]
+                answers = inquirer.prompt(questions)
+                name = answers["name"]
+                passphrase = answers["passphrase"]
+                if len(passphrase.split(" ")) not in [12, 24]:
                     print("\n")
-                    print(f"{Fore.RED}Invalid passphrase. Passphrase should contain 12 or 24 words.{Style.RESET_ALL}")
+                    print(
+                        f"{Fore.RED}Invalid passphrase. Passphrase should contain 12 or 24 words.{Style.RESET_ALL}"
+                    )
+                    input("Press Enter to continue...")  # Pause before clearing
                     continue
 
                 print("\n")
                 private_key = km.to_private_key(passphrase)
                 km.add_key(name, private_key)
+                print(f"Account '{name}' restored successfully.\n")
+                input("Press Enter to continue...")
                 continue
-            case '2':
-                name = input("Enter account name: ")
-                print("\n")
-                km.delete_key(name)
+
+            case "Delete an account":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                accounts = km.load_keys()
+                if accounts:
+                    questions = [
+                        inquirer.List(
+                            "name",
+                            message="Select account to delete",
+                            choices=accounts,
+                        )
+                    ]
+                    answers = inquirer.prompt(questions)
+                    name = answers["name"]
+                    km.delete_key(name)
+                    print(f"Account '{name}' deleted.\n")
+                else:
+                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}\n")
+                input("Press Enter to continue...")
                 continue
-            case '3':
-                name = input("Enter account name: ")
-                private_key = km.get_decrypted_key(name)
-                if private_key is not None:
-                    print(f"Private key: {private_key}")
+
+            case "Get private key from an account":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                accounts = km.load_keys()
+                if accounts:
+                    questions = [
+                        inquirer.List(
+                            "name",
+                            message="Select account to get private key",
+                            choices=accounts,
+                        )
+                    ]
+                    answers = inquirer.prompt(questions)
+                    name = answers["name"]
+                    private_key = km.get_decrypted_key(name)
+                    if private_key is not None:
+                        print(f"Private key for '{name}': {private_key}\n")
+                else:
+                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}\n")
+                input("Press Enter to continue...")
                 continue
-            case '4':
-                num_accounts = int(input("Enter the number of account(s) to generate: "))
-                name_prefix = input("Enter the name prefix for the account(s): ")
+
+            case "Generate new account(s)":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                questions = [
+                    inquirer.Text(
+                        "num_accounts",
+                        message="Enter the number of account(s) to generate",
+                        validate=lambda _, x: x.isdigit() and int(x) > 0,
+                    ),
+                    inquirer.Text("name_prefix", message="Enter the name prefix for the account(s)"),
+                ]
+                answers = inquirer.prompt(questions)
+                num_accounts = int(answers["num_accounts"])
+                name_prefix = answers["name_prefix"]
                 print("\n")
                 for num in range(num_accounts):
-                    name = km.create(name=f"{name_prefix}_{num + 1}")
-                print(f"Successfully generated {num_accounts} account(s).")
-                print("\n")
+                    km.create(name=f"{name_prefix}_{num + 1}")
+                print(f"Successfully generated {num_accounts} account(s).\n")
+                input("Press Enter to continue...")
                 continue
-            case '5':
-                global w3
-                w3 = w3_init(config["ENDPOINT"])
+
+            case "Connect to endpoint":
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+                endpoint_default = config.get("ENDPOINT", "")
+
+                questions = [
+                    inquirer.Password(
+                        "endpoint",
+                        message="Enter Ethereum endpoint (default from .env file):",
+                        default=endpoint_default
+                    )
+                ]
+
+                answers = inquirer.prompt(questions)
+                endpoint = answers["endpoint"] or endpoint_default
+                if not endpoint:
+                    print(f"{Fore.RED}\nNo endpoint provided.{Style.RESET_ALL}\n")
+                    input("Press Enter to continue...")
+                    continue
+                w3 = w3_init(endpoint)
                 print("\n")
+                input("Press Enter to continue...")
                 continue
-            case '6':
-                if len(km.load_keys()) != 0:
+
+            
+            case "Show my accounts":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                accounts = km.load_keys()
+                if accounts:
                     print("Available accounts:")
-                    pointer = 1
-                    for acc in km.load_keys():
-                        print(f"{pointer}. {acc}")
-                        pointer += 1
-                    print("_________________")
-                    print("\n")
+                    for idx, acc in enumerate(accounts, start=1):
+                        print(f"{idx}. {acc}")
+                    print("_________________\n")
                 else:
-                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}")
-                    print("\n")
-            case '7': 
-                if len(km.load_keys()) != 0:
+                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}\n")
+                input("Press Enter to continue...")
+                continue
+
+            case "Get balance of each account":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                accounts = km.load_keys()
+                if accounts:
+                    if w3 is None:
+                        print(f"{Fore.RED}\nPlease connect to the endpoint first.{Style.RESET_ALL}\n")
+                        input("Press Enter to continue...")
+                        continue
                     try:
-                        for acc in km.load_keys():
+                        for acc in accounts:
                             key = km.get_decrypted_key(acc)
                             address = w3.eth.account.from_key(key).address
                             balance = w3.eth.get_balance(address)
                             # Convert the balance from wei to ether
                             print(f"{acc}: {balance / 10**18} ETH")
                         print("\n")
-                    except NameError:
-                        print(f"{Fore.RED}\nPlease connect to the endpoint.{Style.RESET_ALL}")
+                    except Exception as e:
+                        print(f"{Fore.RED}\nError fetching balances: {e}{Style.RESET_ALL}\n")
                 else:
-                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}")
-                    print("\n")
-            
-            case '9':
+                    print(f"{Fore.RED}\nNo accounts found.{Style.RESET_ALL}\n")
+                input("Press Enter to continue...")
+                continue
+
+            case "Exit":
                 print("Exiting the program...")
                 exit(0)
-            case _:
-                print("Invalid choice. Please try again.")
-                print("\n")
 
-    
+            case _:
+                print("Invalid choice. Please try again.\n")
+                input("Press Enter to continue...")
+
+
 if __name__ == "__main__":
     menu()
-
-    
-    
