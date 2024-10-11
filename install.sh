11169979ccb00b97d0d81555affabb461ee813d7
0ndrec <byeexs@gmail.com>
2024-10-11 12:17:12 +0300

    _

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ fi
 # Create virtual environment
 if [[ ! -d "$VENV_DIR" ]]; then
     echo "Creating Python virtual environment..."
-    cd "$APP_DIR" && python3 -m venv "$VENV_POINT" || python -m venv "$VENV_POINT" || error_exit "Failed to create virtual environment."
+    cd "$APP_DIR" && python3 -m venv "$VENV_POINT" || error_exit "Failed to create virtual environment."
 else
     echo "Virtual environment already exists at $VENV_DIR."
 fi
